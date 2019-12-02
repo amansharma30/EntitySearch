10cdd2e7d928cbb7565042cb6ddbd0aa2cf457a3
amansharma <aman8803468878@gmail.com>
2019-12-02 18:38:45 +0100

    Fixed issues in INdexing model.Previous commit must not be considered.

---
+++ EntitySearch/src/main/java/org/indexing/Occurrence.java
@@ -0,0 +1,21 @@
+package org.indexing;
+
+public class Occurrence {
+
+	private int docId;
+	private int pos;
+	
+	public int getDocId() {
+		return docId;
+	}
+	public void setDocId(int docId) {
+		this.docId = docId;
+	}
+	public int getPos() {
+		return pos;
+	}
+	public void setPos(int pos) {
+		this.pos = pos;
+	}
+	
+}
